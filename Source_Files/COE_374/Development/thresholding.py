1433b3e6ee6e453a662bb6737c2c1faa5b422bae
Nicholas Aufiero <31543183+naufiero@users.noreply.github.com>
2022-04-01 13:27:57 -0500

    Updated with working contours

--- Source_Files/COE_374/Development/thresholding.py
+++ Source_Files/COE_374/Development/thresholding.py
@@ -1,18 +1,3 @@
-'''import numpy as np
-import cv2
-from matplotlib import pyplot as plt
-
-img = cv2.imread('it_100.png',0)
-lower_bounds = (100,0,0)
-upper_bounds = (225,80,80)
-mask = cv2.inRange(img, lower_bounds, upper_bounds)
-maskrgb = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
-img = img & maskrgb
-#ret,thresh1 = cv.threshold(img,200,255,cv.THRESH_BINARY)
-
-	
-cv2.imwrite('Test_gray.jpg', img) '''
-
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
@@ -32,22 +17,23 @@ imask = mask>0
 green = np.zeros_like(img, np.uint8)
 green[imask] = img[imask]
 
+cv2.imwrite("green.png", green)
+
 ret,thresh1 = cv2.threshold(green,200,255,cv2.THRESH_BINARY)
 gray = cv2.cvtColor(thresh1, cv2.COLOR_BGR2GRAY)
+
+cv2.imwrite("gray.png", gray)
 
 thresh = 20
 im_bw = cv2.threshold(gray, thresh, 255, cv2.THRESH_BINARY)[1]
 cv2.imwrite('bw_image.png', im_bw)
 # find and draw contours
 
-contours = cv2.findContours(im_bw, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[-2]
-for contour in contours:
-   cv2.drawContours(gray, contour, -1, (0, 255, 0), 3)
-cv2.drawContours(im_bw, contours[0], -1, (0,255,0), 3)
 
-plt.figure()
-plt.imshow(im_bw)
-plt.imsave("test_image.png",im_bw)
+contours = cv2.findContours(im_bw, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+
+cv2.drawContours(img, contours, -1, (0,255,0), 5)
+
+cv2.imwrite("outlined_image.png",img)
 ## save 
-cv2.imwrite("green.png", thresh1)
-
+#cv2.imwrite("green.png", thresh1)
