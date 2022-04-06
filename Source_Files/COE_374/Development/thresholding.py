250634bf6d493a3dcc313816b40725c7a5dd5522
Ryan <ryanarylagan@utexas.edu>
2022-04-06 14:20:37 -0500

    updating thresholding.py with FLANN

--- Source_Files/COE_374/Development/thresholding.py
+++ Source_Files/COE_374/Development/thresholding.py
@@ -88,6 +88,7 @@ cv2.imwrite("outputImages/face.png",face)
 img1 = cv2.imread('inputImages/queryImageSmiley.png',cv2.IMREAD_GRAYSCALE) # queryImage
 img2 = cv2.imread('outputImages/face.png',cv2.IMREAD_GRAYSCALE) # trainImage
 
+
 # Initiate SIFT detector
 sift = cv2.SIFT_create()
 
@@ -95,18 +96,26 @@ sift = cv2.SIFT_create()
 kp1, des1 = sift.detectAndCompute(img1,None)
 kp2, des2 = sift.detectAndCompute(img2,None)
 
-# BFMatcher with default params
-bf = cv2.BFMatcher()
-matches = bf.knnMatch(des1,des2,k=2)
+# FLANN parameters
+FLANN_INDEX_KDTREE = 1
+index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+search_params = dict(checks=100)   # or pass empty dictionary
+flann = cv2.FlannBasedMatcher(index_params,search_params)
+matches = flann.knnMatch(des1,des2,k=2)
 
-# Apply ratio test
-good = []
-for m,n in matches:
-    if m.distance < 0.70*n.distance:
-        good.append([m])
+# Need to draw only good matches, so create a mask
+matchesMask = [[0,0] for i in range(len(matches))]
 
-# cv.drawMatchesKnn expects list of lists as matches.
-img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,good,None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-plt.imshow(img3),plt.show()
+# ratio test as per Lowe's paper
+for i,(m,n) in enumerate(matches):
+    if m.distance < 0.75*n.distance:
+        matchesMask[i]=[1,0]
+draw_params = dict(matchColor = (0,255,0),
+                   singlePointColor = (255,0,0),
+                   matchesMask = matchesMask,
+                   flags = cv2.DrawMatchesFlags_DEFAULT)
+img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,matches,None,**draw_params)
+
+plt.imshow(img3,),plt.show()
 
 cv2.imwrite("outputImages/SIFT_test.png",img3)
