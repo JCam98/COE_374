0f71d5a73d22cae76f9ddd2131439827d9920c85
Ryan <ryanarylagan@utexas.edu>
2022-04-25 18:15:59 -0500

    Updated resize.py to take in a single image at a time

--- Source_Files/COE_374/Development/resize.py
+++ Source_Files/COE_374/Development/resize.py
@@ -6,21 +6,16 @@ import matplotlib.pyplot as plt
 import math
 import sys
 
-input = str(sys.argv[1])
-output = str(sys.argv[2])
-count = 10000
+def resize(frame, count):
 
-for images in os.listdir(input):
-    img = cv2.imread(input + '/' + images, cv2.IMREAD_UNCHANGED)
-    
-    scale_percent = int(sys.argv[3]) # percent of original size
-    width = int(img.shape[1] * scale_percent / 100)
-    height = int(img.shape[0] * scale_percent / 100)
+    count = count + 10001
+    scale_percent = 66 # percent of original size
+    width = int(frame.shape[1] * scale_percent / 100)
+    height = int(frame.shape[0] * scale_percent / 100)
     dim = (width, height)
 
     # resize image
-    resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA) 
-    cv2.imwrite(output +'/resized' +str(count)+'.jpg', resized)
-    count += 1
+    resized = cv2.resize(frame, dim, interpolation = cv2.INTER_AREA) 
+    cv2.imwrite('images/resized/resized' +str(count)+'.jpg', resized)
 
-print('Done. ' + str(count-10000) + " images resized to " + str(scale_percent) + "% of their original resolution.")
+    return frame
